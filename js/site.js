1a3a410c6e9c0d92857bbf0a45549b19da23ad3e
Tybook92 <112991709+Tybook92@users.noreply.github.com>
2023-02-11 22:24:06 -0600

    added java portion

--- js/site.js
+++ js/site.js
@@ -1,4 +1,26 @@
-//Call Hello World
-function helloWorld() {
-    alert("hellow World");
+//get the values from the Page
+//starts or controller function
+function getValues(){
+    //getvalues from the page
+    let startValue = document.getElementById("startValue").value;
+    let endValue = document.getElementById("endValue").value;
+    
+    //we call generateNumbers
+    let number = generateNumbers()
+
+    //we call displayNumbers
+}
+
+//generate numbers from the startvalue to the endValue
+//logic function(s)
+function generateNumbers(startValue, endValue){
+    let numbers = [];
+
+    return numbers;
+}
+
+//display the numbers and mark even numbers bold
+//display or view functions
+function displayNumbers(){
+
 }
